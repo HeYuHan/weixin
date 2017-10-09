40a62a3d769a471c2e83b4e6df41fecfe89dd8e9
HeYuHan <435481457@qq.com>
2017-10-09 14:57:49 +0800

    verify

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+router.get('/VerifyToken',function(req,res,next){
+  console.log(req.body);
+});
 module.exports = router;
