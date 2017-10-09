8379b25f898b6ea54e531b5d541544466c9589f8
HeYuHan <435481457@qq.com>
2017-10-09 16:27:18 +0800

    sig

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,26 @@
 var express = require('express');
 var router = express.Router();
-
+var crypto = require('crypto');
 /* GET home page. */
+function checkSignature(query){
+  var sig = query.signature;
+  
+  var timestamp = query.timestamp;
+  var nonce = query.nonce;
+  var arry = ["wohnb",timestamp,nonce];
+  arry.sort();
+  var content = arry[0]+arry[1]+arry[2];
+  var shasum = crypto.createHash('sha1');
+  shasum.update(content);
+  return sig == shasum.digest('hex');
+}
 router.get('/', function(req, res, next) {
-  var sig = req.signature;
-  var echo = req.echostr;
-  var timestamp = req.timestamp;
-  var nonce = req.nonce;
-  console.log(req.query);
-  res.end(echo);
+  var query = req.query;
+  if(!checkSignature(query))
+  {
+    res.end(query.echostr);
+  }
+  console.log(query);
+  
 });
 module.exports = router;
